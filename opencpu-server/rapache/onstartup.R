93c22b4ee5bac65694964f368f30311349dbf019
Jeroen <jeroenooms@gmail.com>
2013-07-23 22:54:12 +0200

    lots of updates

---
+++ opencpu-server/rapache/onstartup.R
@@ -0,0 +1,16 @@
+#Disable this for development
+.libPaths('/usr/lib/opencpu/library');
+
+#try to disable interactivity
+try(.Call(parallel:::C_mc_interactive, FALSE));
+
+#override temp directory
+dir.create("/tmp/ocpu-temp", showWarnings = FALSE, recursive = TRUE, mode = "0777");
+unixtools::set.tempdir("/tmp/ocpu-temp");
+options(configure.vars = paste0("TMPDIR=", tempdir()));
+options(rapache=TRUE);
+
+#Set some hw limits
+RAppArmor::rlimit_as(1024^3, verbose=TRUE);
+RAppArmor::rlimit_nproc(50, verbose=TRUE);
+RAppArmor::aa_change_profile("ocpu-main");
