d3eaefdcd83bd6b5eec5d54dd5b92d6d6f649c7a
Jeroen <jeroenooms@gmail.com>
2013-07-27 14:08:39 +0200

    lots of changes

--- opencpu-server/rapache/onstartup.R
+++ opencpu-server/rapache/onstartup.R
@@ -4,19 +4,16 @@
 #try to disable interactivity
 try(.Call(parallel:::C_mc_interactive, FALSE));
 
-#override temp directory
-dir.create("/tmp/ocpu-temp", showWarnings = FALSE, recursive = TRUE, mode = "0777");
-unixtools::set.tempdir("/tmp/ocpu-temp");
-
-#These are needed for install packages
-Sys.setenv(TMPDIR = "/tmp/ocpu-temp")
-options(configure.vars = paste0("TMPDIR=", tempdir()));
-
 #we use this later
 options(rapache=TRUE);
 
 #Load RAppArmor while it is in .libPaths.
 getNamespace("RAppArmor")
+getNamespace("unixtools")
+
+#Better defer overriding tempdir to later
+#dir.create("/tmp/ocpu-temp", showWarnings = FALSE, recursive = TRUE, mode = "0777");
+#unixtools::set.tempdir("/tmp/ocpu-temp");
 
 #Better defer HW limits to later on (eval.secure).
 #RAppArmor::rlimit_as(1024^3, verbose=TRUE);
