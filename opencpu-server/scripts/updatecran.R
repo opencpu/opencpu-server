85dd504172604119c9d95aeaa4a8b9ef5cf74e2a
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-15 16:18:23 -0800

    updates

--- opencpu-server/scripts/updatecran.R
+++ opencpu-server/scripts/updatecran.R
@@ -1,5 +1,2 @@
-#CRAN packages
-update.packages(lib.loc = "/tmp/ocpu-www-data/cran_library/", repos = "http://cran.r-project.org", ask = FALSE, checkBuilt=TRUE);
-if(length(list.files("/tmp/ocpu-www-data/cran_library/"))){
-	system2("touch", "/tmp/ocpu-www-data/cran_library/*");
-}
+.libPaths("/usr/lib/opencpu/library"); 
+opencpu:::updatecran();
