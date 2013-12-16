464c9223010ea8f18065aa50135f874a6f69b226
Jeroen Ooms <jeroenooms@gmail.com>
2013-12-15 20:23:06 -0800

    fixing update scripts

--- opencpu-server/scripts/updatebioc.R
+++ opencpu-server/scripts/updatebioc.R
@@ -1,2 +1,13 @@
+#use the system lib
 .libPaths("/usr/lib/opencpu/library"); 
+
+#Load RAppArmor while it is in .libPaths.
+getNamespace("RAppArmor")
+getNamespace("unixtools")
+
+#load opencpu. This triggers initiating config, tempdir, etc.  
+options(rapache=TRUE);
+library(opencpu)
+
+#update bioc
 opencpu:::updatebioc();
--- opencpu-server/scripts/updategithub.R
+++ opencpu-server/scripts/updategithub.R
@@ -1,2 +1,13 @@
+#use the system lib
 .libPaths("/usr/lib/opencpu/library"); 
+
+#Load RAppArmor while it is in .libPaths.
+getNamespace("RAppArmor")
+getNamespace("unixtools")
+
+#load opencpu. This triggers setting of proper tempdir, etc. 
+options(rapache=TRUE);
+library(opencpu)
+
+#update github
 opencpu:::updategithub();
