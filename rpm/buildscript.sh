46e4b314b6260159e3e4c4c599cb1c0c32b022af
Jeroen Ooms <jeroenooms@gmail.com>
2014-08-13 14:14:47 +0200

    clean

--- rpm/buildscript.sh
+++ rpm/buildscript.sh
@@ -1,4 +1,4 @@
-# Enable EPEL repository. Only required on REDHAT/CENTOS:
+# Enable EPEL repository. Only required on REDHAT/CENTOS, not on Fedora.
 sudo su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm'
 
 # Update system
@@ -6,30 +6,22 @@ sudo yum update -y
 sudo yum upgrade -y
 
 # rpm dependencies
-sudo yum install -y rpm-build
-
-# rApache dependencies
-sudo yum install -y make wget httpd-devel libapreq2-devel R-devel
-
-# opencpu dependencies
-sudo yum install -y libcurl-devel protobuf-devel
+sudo yum install -y rpm-build make wget httpd-devel libapreq2-devel R-devel libcurl-devel protobuf-devel
 
 # setup dirs
 mkdir -p ~/rpmbuild/SOURCES
 mkdir -p ~/rpmbuild/SPECS
 
-# Get the rapache source code
+# Get the rapache sources
 wget https://github.com/jeffreyhorner/rapache/archive/v1.2.6.tar.gz -O rapache-1.2.6.tar.gz
 tar xzvf rapache-1.2.6.tar.gz rapache-1.2.6/rpm/rapache.spec --strip-components 2
-
-# Move to build dirs
 mv -f rapache-1.2.6.tar.gz ~/rpmbuild/SOURCES/
 mv -f rapache.spec ~/rpmbuild/SPECS/
 
 # Build rApache
 rpmbuild -ba ~/rpmbuild/SPECS/rapache.spec
 
-# Download (and rename) opencpu source archive
+# Get opencpu sources
 wget https://github.com/jeroenooms/opencpu-server/archive/v1.4.4.tar.gz -O opencpu-server-1.4.4.tar.gz
 tar xzvf opencpu-server-1.4.4.tar.gz opencpu-server-1.4.4/rpm/opencpu.spec --strip-components 2
 mv -f opencpu-server-1.4.4.tar.gz ~/rpmbuild/SOURCES/
@@ -38,13 +30,13 @@ mv -f opencpu.spec ~/rpmbuild/SPECS/
 # Build OpenCPU
 rpmbuild -ba ~/rpmbuild/SPECS/opencpu.spec
 
-# Install
+# Install OpenCPU
 sudo yum install mod_ssl /usr/sbin/semanage
 cd ~/rpmbuild/RPMS/x86_64/
 sudo rpm -i rapache-*.rpm
 sudo rpm -i opencpu-lib-*.rpm
 sudo rpm -i opencpu-server-*.rpm #takes a while!
 
-# Test
+# Test OpenCPU
 curl http://localhost/ocpu/library/
 
