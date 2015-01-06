e22e4e87cb3d1a13a0546508950cf39b95dbc1a5
Jeroen Ooms <jeroenooms@gmail.com>
2015-01-06 10:23:39 -0800

    fix

--- opencpu-server/scripts/cleanocpu.sh
+++ opencpu-server/scripts/cleanocpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #This script removes all tempfiles that are over an hour old
-find /tmp/ocpu-temp/ -mmin +60 -user www-data -delete || true
+find /tmp/ocpu-temp/ -mindepth 1 -mmin +60 -user www-data -delete || true
 find /tmp/ocpu-temp/ -mindepth 1 -mmin +60 -user www-data -type d -empty -exec rmdir {} \; || true
 
 #This removes entries from the "temporary library" over a day old.
-find /tmp/ocpu-www-data/tmp_library/ -mmin +1440 -user www-data -delete || true
+find /tmp/ocpu-www-data/tmp_library/ -mindepth 1 -mmin +1440 -user www-data -delete || true
 find /tmp/ocpu-www-data/tmp_library/ -mindepth 1 -mmin +1440 -user www-data -type d -empty -exec rmdir {} \; || true
