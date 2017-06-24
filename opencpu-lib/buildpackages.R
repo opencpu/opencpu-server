0d692c7c07d790c1305fcb29eb62678ae88b8282
Jeroen Ooms <jeroenooms@gmail.com>
2017-06-24 12:42:26 -0400

    update stack for 2.0.3 release

--- opencpu-lib/buildpackages.R
+++ opencpu-lib/buildpackages.R
@@ -1,7 +1,7 @@
 #Remove local and home libraries to make sure we only depends on base and recommended packages.
 #Note: This is still not safe in rpm because base and cran packages are in the same dir.
 baselib <- grep("^/usr/lib(64)?/R/library", .libPaths(), value=TRUE)
-assign(".lib.loc", baselib, envir=environment(.libPaths));
+assign(".lib.loc", baselib, envir=environment(.libPaths))
 
 #Load Rcpp (to compile httpuv)
 #Note that httpuv will not be used by rApache. However it is needed to test-load the package after installation.
@@ -10,27 +10,19 @@ assign(".lib.loc", baselib, envir=environment(.libPaths));
 
 #this dir contains the source packages
 sourcedir <- file.path(getwd(), "opencpu-lib")
-destdir <- file.path(sourcedir, "build");
+destdir <- file.path(sourcedir, "build")
 stopifnot(dir.create(destdir))
 
 #we fist need to create a package index
 library(tools);
 write_PACKAGES(sourcedir)
 
-#For Fedora/Redhat
-if(is.na(Sys.getenv("NO_APPARMOR", NA))){
-	message("Building with AppArmor support")
-} else {
-	message("Building without AppArmor support")
-	options(configure.vars="NO_APPARMOR=1")
-}
-
 # In case of packages installed elsewhere
 library(methods)
 environment(.libPaths)$.lib.loc = character(0)
 
 #Because of dependencies=TRUE, suggested packages sendmailR, unix and RProtoBuf are also installed.
-install.packages(c("opencpu", "RAppArmor"), dependencies=TRUE, type="source", lib=destdir, contriburl=paste0("file://", sourcedir));
+install.packages("opencpu", dependencies=TRUE, type="source", lib=destdir, contriburl=paste0("file://", sourcedir))
 
 # This causes problems if some packages were pre-installed in the root global libarary
 # remove.packages("BH", lib = destdir)
