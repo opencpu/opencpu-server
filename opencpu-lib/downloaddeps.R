193c750834c8c5c48aab95ffe6eb3794275d4417
Jeroen Ooms <jeroenooms@gmail.com>
2019-10-23 11:56:38 +0200

    Stack update

--- opencpu-lib/downloaddeps.R
+++ opencpu-lib/downloaddeps.R
@@ -3,4 +3,4 @@ deps <- tools::package_dependencies('opencpu', which = 'most')$opencpu
 deps <- deps[!deps %in% c('sendmailR', 'R.rsp')]
 deps2 <- tools::package_dependencies(deps, recursive = TRUE)
 alldeps <- sort(unique(c(deps, unlist(deps2))))
-download.packages(alldeps, destdir = '.')
+download.packages(c('opencpu', alldeps), destdir = '.')
--- rpm/opencpu.spec
+++ rpm/opencpu.spec
@@ -3,7 +3,7 @@
 
 Name: opencpu
 Version: 2.1.4
-Release: rpm0
+Release: rpm1
 Source: opencpu-server-%{branch}.tar.gz
 License: Apache2
 Summary: The OpenCPU system for embedded scientific computing and reproducible research.
