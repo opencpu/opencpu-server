406d8810a96c610a3a2e5492b41cf38910542168
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-27 13:08:24 +0200

    Update script cleanocpu.sh to long paths.

--- opencpu-server/scripts/cleanocpu.sh
+++ opencpu-server/scripts/cleanocpu.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+ls /tmp | grep apache2.service > work.txt
+temp="/tmp/$(cat work.txt)/tmp/ocpu-temp"
+store="/tmp/$(cat work.txt)/tmp/ocpu-store"
+
 #This script removes all tempfiles that are over an hour old
-if [ -d "/tmp/ocpu-temp" ]; then
-    find /tmp/ocpu-temp/ -mindepth 1 -mmin +60 -user www-data -delete || true
-    find /tmp/ocpu-temp/ -mindepth 1 -mmin +60 -user www-data -type d -empty -exec rmdir {} \; || true
+if [ -d "$temp" ]; then
+    find $temp -mindepth 1 -mmin +60 -user www-data -delete || true
+    find $temp -mindepth 1 -mmin +60 -user www-data -type d -empty -exec rmdir {} \; || true
 fi
 
 #This removes entries from the "temporary library" over a day old.
-if [ -d "/tmp/ocpu-store" ]; then
-    find /tmp/ocpu-store/ -mindepth 1 -mmin +1440 -user www-data -delete || true
-    find /tmp/ocpu-store/ -mindepth 1 -mmin +1440 -user www-data -type d -empty -exec rmdir {} \; || true
+if [ -d "$store" ]; then
+    find $store -mindepth 1 -mmin +1440 -user www-data -delete || true
+    find $store -mindepth 1 -mmin +1440 -user www-data -type d -empty -exec rmdir {} \; || true
 fi
