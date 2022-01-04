7cba9f7e627b1f9d952d927240ed5e12c2ea3b41
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-05 00:23:21 +0100

    Lookup apache2 PrivateTmp Closes #11

--- opencpu-server/scripts/cleanocpu.sh
+++ opencpu-server/scripts/cleanocpu.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+tmp="/tmp"
 
-#This script removes all tempfiles that are over an hour old
-if [ -d "/tmp/ocpu-temp" ]; then
-    find /tmp/ocpu-temp/ -mindepth 1 -mmin +60 -user www-data -delete || true
-    find /tmp/ocpu-temp/ -mindepth 1 -mmin +60 -user www-data -type d -empty -exec rmdir {} \; || true
+# Test if we are running systemd
+if [ -d "/run/systemd/system" ]; then
+PID=$(systemctl show --property MainPID --value apache2)
+if [ "$PID" ] && [ $PID -ne 0 ] && [ -d "/proc/$PID/root/tmp" ]; then
+tmp="/proc/$PID/root/tmp"
+fi
+fi
+ocputemp="$tmp/ocpu-temp"
+ocpustore="$tmp/ocpu-store"
+
+#This script removes tempfiles that are over an hour old
+if [ -d "$ocputemp" ]; then
+    find "$ocputemp" -mindepth 1 -mmin +60 -user www-data -delete || true
+    find "$ocputemp" -mindepth 1 -mmin +60 -user www-data -type d -empty -exec rmdir {} \; || true
 fi
 
 #This removes entries from the "temporary library" over a day old.
-if [ -d "/tmp/ocpu-store" ]; then
-    find /tmp/ocpu-store/ -mindepth 1 -mmin +1440 -user www-data -delete || true
-    find /tmp/ocpu-store/ -mindepth 1 -mmin +1440 -user www-data -type d -empty -exec rmdir {} \; || true
+if [ -d "$ocpustore" ]; then
+    find "$ocpustore" -mindepth 1 -mmin +1440 -user www-data -delete || true
+    find "$ocpustore" -mindepth 1 -mmin +1440 -user www-data -type d -empty -exec rmdir {} \; || true
 fi
